c62a9489679d67f4669d45e16cc546f8c763087a
Eugen Dahm <eugen.dahm@gmail.com>
2021-06-10 23:11:28 +0200

    Remove unneeded testcontainer config.

--- src/test/java/com/github/frostbittenking/pgjdbcBugDemo/TestConfiguration.java
+++ src/test/java/com/github/frostbittenking/pgjdbcBugDemo/TestConfiguration.java
@@ -4,11 +4,8 @@ import com.github.frostbittenking.pgjdbcBugDemo.persistence.jpa.entity.EntityBas
 import com.github.frostbittenking.pgjdbcBugDemo.persistence.jpa.repository.DemoJpaModule;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
@@ -18,15 +15,4 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @EnableJpaRepositories(basePackageClasses = DemoJpaModule.class)
 
 public class TestConfiguration {
-
-    @Container
-    private PostgreSQLContainer postgreSQLContainer = new PGJDBCSQLContainer("postgres:11.8-alpine")
-            .withDatabaseName("pgjdbcdemo")
-            .withUsername("pgjdbcdemo")
-            .withPassword("pgjdbcdemo");
-
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public PostgreSQLContainer postgreSQLContainer() {
-        return postgreSQLContainer;
-    }
 }
